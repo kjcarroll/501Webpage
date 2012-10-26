5a0ba35c81598f6a0599411275c121f41476fe26
Brian Balsamo <Falx4444+git@gmail.com>
2012-10-26 14:46:31 -0500

    script now takes an argument, instead of requiring manual piping into an XML document

--- xkcdXMLarchive.sh
+++ xkcdXMLarchive.sh
@@ -3,6 +3,13 @@
 #Requires jsawk and wget, other than that I think the rest of these are fairly standard GNU utils than everyone should have...
 #I could probably get rid of the wget dep and just use curl, but wheres the fun in that?
 
+#Be sure we get the info we need, and not other junk
+if [ -z $1 ]; then echo "Reqires the name of your XML file as an argument" && exit 1; fi
+if [ -n $2 ]; then echo "Only one argument allowed" && exit 1; fi
+
+#DANGER
+if [ -a $1 ]; then echo "It looks like $1 already exists. Manually move or rename it to recreate a database under that name" && exit 1; fi
+
 MAXNUM="`wget -q -O - http://xkcd.com/info.0.json | jsawk 'return this.num'`"
 echo -ne "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<comicRoot>\n"
 for ((i=1;i<=$MAXNUM;i++))
@@ -21,6 +28,6 @@ for ((i=1;i<=$MAXNUM;i++))
         TITLE=`echo $TITLE | sed 's/</\&lt;/g'`
         TITLE=`echo $TITLE | sed 's/>/\&gt;/g'`
         TITLE=`echo $TITLE | sed 's/&/\&amp;/g'`
-        echo -e "\t<entry>\n\t\t<title>$TITLE</title>\n\t\t<number>$NUMBER</number>\n\t\t<imageLink>$LINK</imageLink>\n\t\t<transcript>$TRANSCRIPT</transcript>\n\t\t<date>$DATE</date>\n\t</entry>"
+        echo -e "\t<entry>\n\t\t<title>$TITLE</title>\n\t\t<number>$NUMBER</number>\n\t\t<imageLink>$LINK</imageLink>\n\t\t<transcript>$TRANSCRIPT</transcript>\n\t\t<date>$DATE</date>\n\t</entry>" >> $1
     done
-echo -e "</comicRoot>"
+echo -e "</comicRoot>" >> $1
